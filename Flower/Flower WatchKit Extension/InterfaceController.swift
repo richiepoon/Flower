eba86acb82d7bd93859cefc971fa85ac139ceeb0
ucirichiep <richiep@uci.edu>
2019-01-27 16:25:08 -0800

    sunday DONE!

--- Flower/Flower WatchKit Extension/InterfaceController.swift
+++ Flower/Flower WatchKit Extension/InterfaceController.swift
@@ -28,19 +28,64 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
     
+    var total_rounds = 2.0
+    var current_round = 0.0
+    
     var up = "red"
     var down = "blue"
     var right = "green"
     var left = "yellow"
     var current_image = "green"
     
+    var press_time = 0.0
+    var total_press_time = 0.0
+    var successful_swipes = 0.0
+    var total_swipes = 0.0
+    
     var timer = Timer()
     var timer_open = Timer()
     var timer_reset = Timer()
+    var timer_press = Timer()
     
     var seconds = 30.0
     
+    @IBOutlet var Welcome: WKInterfaceLabel!
+    @IBOutlet var Initial: WKInterfaceLabel!
+    @IBAction func InitialTap(_ sender: Any) {
+        Welcome.setHidden(true)
+        Initial.setHidden(true)
+        Instructions.setHidden(false)
+    }
+    @IBOutlet var Instructions: WKInterfaceLabel!
+    @IBAction func InstructionsTap(_ sender: Any) {
+        Instructions.setHidden(true)
+        closed.setHidden(false)
+        Tap.isEnabled = true
+        closed.setImageNamed("flower_guide")
+    }
+    
+    
     @IBOutlet var closed: WKInterfaceImage!
+    @IBOutlet var Results_label: WKInterfaceLabel!
+    @IBOutlet var Accuracy_label: WKInterfaceLabel!
+    @IBOutlet var Press_label: WKInterfaceLabel!
+    
+    @IBOutlet var Restart_button: WKInterfaceButton!
+    @IBAction func Restart_button_Action() {
+        Results_label.setHidden(true)
+        Accuracy_label.setHidden(true)
+        Press_label.setHidden(true)
+        Restart_button.setHidden(true)
+        Welcome.setHidden(false)
+        Initial.setHidden(false)
+        current_round = 0.0
+        press_time = 0.0
+        total_press_time = 0.0
+        successful_swipes = 0.0
+        total_swipes = 0.0
+        seconds = 30.0
+        
+    }
     
     @objc func counter(){
         print(seconds)
@@ -65,9 +110,9 @@ class InterfaceController: WKInterfaceController {
     
     @objc func counter_reset(){
         print(seconds)
-        closed.setAlpha(CGFloat(seconds/20.0))
+        closed.setAlpha(CGFloat(seconds/10.0))
         seconds += 1
-        if (seconds >= 20){
+        if (seconds >= 10){
             print("reset")
             timer_reset.invalidate()
             longPress.isEnabled = true
@@ -79,9 +124,14 @@ class InterfaceController: WKInterfaceController {
         }
     }
     
+    @objc func counter_press(){
+        press_time += 0.1
+    }
+    
     @IBOutlet var longPress: WKLongPressGestureRecognizer!
     @IBAction func longPressAction(_ gestureRecognizer: WKLongPressGestureRecognizer) {
         if gestureRecognizer.state == .began{
+            timer_press = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector (InterfaceController.counter_press), userInfo: nil, repeats: true)
             timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector (InterfaceController.counter), userInfo: nil, repeats: true)
             //DispatchQueue.main.asyncAfter(1.0)
         }
@@ -90,15 +140,36 @@ class InterfaceController: WKInterfaceController {
             closed.setAlpha(0.0)
             randomize_color()
             timer_open = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector (InterfaceController.counter_open), userInfo: nil, repeats: true)
+            timer_press.invalidate()
+            total_press_time += press_time
+            press_time = 0
         }
-
     }
     
     @objc func reset(){
-        closed.setImageNamed("lotus_closed")
-        closed.setAlpha(0.0)
-        seconds = 0
-        timer_reset = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector (InterfaceController.counter_reset), userInfo: nil, repeats: true)
+        current_round += 1
+        print(current_round)
+        if (current_round > total_rounds){
+            longPress.isEnabled = false
+            SwipeUp.isEnabled = false
+            SwipeDown.isEnabled = false
+            SwipeRight.isEnabled = false
+            SwipeLeft.isEnabled = false
+            closed.setHidden(true)
+            Results_label.setHidden(false)
+            Accuracy_label.setText("Correct Swipes: " + String(format: "%.2f", (successful_swipes/total_swipes)*100) + "%")
+            Accuracy_label.setHidden(false)
+            Press_label.setText("Average Press Time: " + String(format: "%.2f", (total_press_time/total_rounds)) + "s")
+            Press_label.setHidden(false)
+            Restart_button.setHidden(false)
+            print("end")
+        }
+        else{
+            closed.setImageNamed("lotus_closed")
+            closed.setAlpha(0.0)
+            seconds = 0
+            timer_reset = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector (InterfaceController.counter_reset), userInfo: nil, repeats: true)
+        }
     }
     
     @objc func randomize_color(){
@@ -139,38 +210,51 @@ class InterfaceController: WKInterfaceController {
     @IBAction func SwipeRightAction(_ sender: Any) {
         if (current_image == right){
             WKInterfaceDevice.current().play(.success)
+            successful_swipes += 1
+            total_swipes += 1
             reset()
         }
         else{
             WKInterfaceDevice.current().play(.failure)
+            total_swipes += 1
         }
+        
         
     }
     @IBAction func SwipeUpAction(_ sender: Any) {
         if (current_image == up){
             WKInterfaceDevice.current().play(.success)
+            successful_swipes += 1
+            total_swipes += 1
             reset()
         }
         else{
             WKInterfaceDevice.current().play(.failure)
+            total_swipes += 1
         }
     }
     @IBAction func SwipeDownAction(_ sender: Any) {
         if (current_image == down){
             WKInterfaceDevice.current().play(.success)
+            successful_swipes += 1
+            total_swipes += 1
             reset()
         }
         else{
             WKInterfaceDevice.current().play(.failure)
+            total_swipes += 1
         }
     }
     @IBAction func SwipeLeftAction(_ sender: Any) {
         if (current_image == left){
             WKInterfaceDevice.current().play(.success)
+            successful_swipes += 1
+            total_swipes += 1
             reset()
         }
         else{
             WKInterfaceDevice.current().play(.failure)
+            total_swipes += 1
         }
     }
 }
